21f840e34b281cceb621cbbc28049c0a58e2dade
Marcelo Torres <marcelotorres1982@gmail.com>
2024-05-14 00:12:23 -0300

    Add files via upload

--- greetings/greetins.go
+++ greetings/greetins.go
@@ -1,14 +1,38 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
+
+	"math/rand"
 )
 
 // Hello irá retornar uma saudação com o nome da pessoa
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//Se nenhum nome for fornecido, retorna uma mensagem de erro
 	//Retorna a saudação que incorpora o nome em uma mensagem
-	message := fmt.Sprintf("Olá, %v. Bem vindo(a)!", name)
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
-	return message
+	//Cria uma mensagem usando formato randômico
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// randomFormat retorna uma de um conjunto de mensagens de saudação. A // mensagem retornada é selecionada aleatoriamente.
+func randomFormat() string {
+	//Uma fatia de formato de mensagem
+	formats := []string{
+		"Olá, %v! Bem vindo(a)",
+		"Que bom ver você, %v",
+		"Salve, %v! Bem vindo(a)",
+	}
+	// Retorna um formato de mensagem selecionado aleatoriamente especificando
+	// um índice aleatório para a fatia de formatos.
+
+	return formats[rand.Intn(len(formats))]
+
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Giovanna")
+	// Define propriedades do Logger predefinido, incluindo
+	// o prefixo de entrada de log e um sinalizador para desabilitar a impressão
+	// da hora, do arquivo de origem e do número da linha.
+
+	// Solicita uma mensagem de saudação.
+	message, err := greetings.Hello("Giovanna")
+
+	// Se um erro foi retornado, imprima-o no console e
+	// saia do programa.
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Se nenhum erro foi retornado, imprima a mensagem retornada
+	// no console.
+
 	fmt.Println(message)
 }
